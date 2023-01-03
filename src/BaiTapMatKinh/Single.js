905db062f1dd00d237ac6d16c75f202a7c802179
Son Bui <buisynamson@gmail.com>
2023-01-03 20:25:26 +0700

    update

--- src/BaiTapMatKinh/Single.js
+++ src/BaiTapMatKinh/Single.js
@@ -7,11 +7,11 @@ export default function Single(props) {
       <div className={styles.card_item}>
         <img className={styles.model} src="./glassesImage/model.jpg" />
         <img src={props.glass.url} className={styles.card_img} />
-        <div className="card-body">
-          <h5 className="card-title bg-info p-4">
+        <div className={styles.info}>
+          <h5 >
             {props.glass.name}
           </h5>
-          <p className={styles.card_text}>{props.glass.desc}</p>
+          <p>{props.glass.desc}</p>
         </div>
       </div>
     </React.Fragment>
